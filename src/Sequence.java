8fdb4e4aedb3384fe3ece7134f42621b3082e7cb
ldough1 <luke.dough1@gmail.com>
2018-02-17 22:54:08 -0500

    algorithms complete

--- src/Sequence.java
+++ src/Sequence.java
@@ -8,6 +8,50 @@
  *
  * @author ldough
  */
+
+import java.util.ArrayList;
+
 public class Sequence {
+    
+    static void Sequence() {}
+    static int count = 0;
+    
+    public static int computeIterative(int n) {
+        int val1 = 0;
+        int val2 = 0;
+        int val3 = 0;
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        for (int i = 0; i <= n; i++) {
+            if (i < 3) {
+                array.add(i);
+            } else {
+                val1 = array.get(i - 1);
+                val2 = array.get(i - 2);
+                val3 = val1 * 2 + val2;
+                array.add(val3);
+            }
+            count++;
+        }
+        
+        
+        return array.get(array.size() - 1);
+    }
+    
+    public static int computeRecursive(int n) {
+        count++;
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        else{
+            return 2 * computeRecursive(n - 1) + computeRecursive(n - 2);
+        }
+    }
+    
+    public static int getCount() {
+        return count;
+    }
     
 }
---
+++ src/Test.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ldough
+ */
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        int result1;
+        int count1;
+        
+        result1 = Sequence.computeIterative(23);
+        count1 = Sequence.getCount();
+        System.out.println("Iterative Result:");
+        System.out.println(result1);
+        System.out.println("Iterative Count:");
+        System.out.println(count1);
+        
+        int result2;
+        int count2;
+
+        result2 = Sequence.computeRecursive(23);
+        count2 = Sequence.getCount();
+        System.out.println("Recursive Result:");
+        System.out.println(result2);
+        System.out.println("Recursive Count:");
+        System.out.println(count2);
+    }
+    
+}
